2e05d4f01b4726bd7749f02691fe6be89a40c35d
Thomas <60454907+Temberlane@users.noreply.github.com>
2026-08-12 16:38:06 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # approach #1 iterate over strs, and check for anagrams matching,
+        # would be very inefficient
+
+        # approach #2, have a set of Counters of strs, if 2 counters are equal, they are anagrams. 
+        # iterate over each then sort have keys its n^2 log n time complexity
+        seen = {} # key : sorted, value : the strings in an array
+        for word in strs:
+            cur_key = "".join(sorted(word))
+            if cur_key not in seen:
+                seen[cur_key] = []    
+            seen[cur_key].append(word)
+            
+        output = []
+        for anagram_group in seen.keys():
+            output.append(seen[anagram_group])
+        return output
